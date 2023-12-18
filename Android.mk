8b7fe42c5d3709a4aafc39a789e18ba517a5c915
picasso09 <119201284+picasso09@users.noreply.github.com>
2023-12-19 02:45:26 +0700

    remove useless maybe

--- Android.mk
+++ Android.mk
@@ -15,68 +15,6 @@ ifeq ($(notdir $(LOCAL_PATH)),$(strip $(LINUX_KERNEL_VERSION)))
 ifneq ($(strip $(TARGET_NO_KERNEL)),true)
 include $(LOCAL_PATH)/kenv.mk
 
-#ifdef VENDOR_EDIT
-#Yongpei.Yao@PSW.MM.Audio.Machine, 2019/11/4, add for 19551 fm config
-ifneq ($(filter oppo6779_18073 oppo6779_18593, $(OPPO_TARGET_DEVICE)),)
-$(shell sed -i 's/CONFIG_MTK_FM_CHIP=*.*/CONFIG_MTK_FM_CHIP=\"MT6635_FM\"/g' $(KERNEL_CONFIG_FILE))
-else
-$(shell sed -i 's/CONFIG_MTK_FM_CHIP=*.*/CONFIG_MTK_FM_CHIP=\"MT6631_FM\"/g' $(KERNEL_CONFIG_FILE))
-endif
-#endif VENDOR_EDIT
-
-#ifdef VENDOR_EDIT
-#Zhijun.ye@PSW.MM.Display.LCD.Stability, 2019/11/16, add for 19301 lcm config
-ifneq ($(filter oppo6779_18073 oppo6779_18593 oppo6779_19301 oppo6779_19011, $(OPPO_TARGET_DEVICE)),)
-$(shell sed -i 's/CONFIG_LCM_HEIGHT=*.*/CONFIG_LCM_HEIGHT=\"2340\"/g' $(KERNEL_CONFIG_FILE))
-else
-#ifdef ODM_WT_EDIT
-#Wu.weihong@ODM_WT.NW.Config, 2020/03/28, LCD bringup, sync kernel oppo6765_defconfig CONFIG_LCM_HEIGHT
-$(shell sed -i 's/CONFIG_LCM_HEIGHT=*.*/CONFIG_LCM_HEIGHT=\"1600\"/g' $(KERNEL_CONFIG_FILE))
-#endif
-endif
-#endif VENDOR_EDIT
-
-#ifdef VENDOR_EDIT
-#oujinrong@BSP.Fingerprint.Secure, 2019/12/25, add for widevine compatible
-ifneq ($(filter oppo6779_19551 oppo6779_18593 oppo6779_19301, $(OPPO_TARGET_DEVICE)),)
-$(shell sed -i 's/CONFIG_MTK_DRM_KEY_MNG_SUPPORT=.*/CONFIG_MTK_DRM_KEY_MNG_SUPPORT=y/g' $(KERNEL_CONFIG_FILE))
-$(shell sed -i 's/CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT=.*/CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT=y/g' $(KERNEL_CONFIG_FILE))
-else
-$(shell sed -i 's/CONFIG_MTK_DRM_KEY_MNG_SUPPORT=.*//g' $(KERNEL_CONFIG_FILE))
-$(shell sed -i 's/CONFIG_MTK_SEC_VIDEO_PATH_SUPPORT=.*//g' $(KERNEL_CONFIG_FILE))
-endif
-#endif VENDOR_EDIT
-
-#ifdef VENDOR_EDIT
-#hongxiang.jin@PSW.MM.Audio.Machine, 2019/11/12, add for 18073 soundtrigger config
-ifneq ($(filter oppo6779_18073, $(OPPO_TARGET_DEVICE)),)
-$(shell sed -i 's/# CONFIG_SND_SOC_DBMDX is not set/CONFIG_SND_SOC_DBMDX=y/g' $(KERNEL_CONFIG_FILE))
-else
-$(shell sed -i 's/CONFIG_SND_SOC_DBMDX=y/# CONFIG_SND_SOC_DBMDX is not set/g' $(KERNEL_CONFIG_FILE))
-endif
-#endif VENDOR_EDIT
-
-#ifdef VENDOR_EDIT
-#weiriqin@camera,driver, 2019/11/18, add for 18073 camera config
-ifneq ($(filter oppo6779_18073 oppo6779_18593, $(OPPO_TARGET_DEVICE)),)
-$(shell sed -i 's/CONFIG_CUSTOM_KERNEL_IMGSENSOR=*.*/CONFIG_CUSTOM_KERNEL_IMGSENSOR=\"imx586p2q_mipi_raw s5kgd1sp_mipi_raw gc5035_mipi_raw\"/g' $(KERNEL_CONFIG_FILE))
-endif
-#endif
-
-#ifdef VENDOR_EDIT
-#weiriqin@camera,driver, 2019/11/18, add for 19301 camera config
-ifneq ($(filter oppo6779_19301 oppo6779_19011, $(OPPO_TARGET_DEVICE)),)
-$(shell sed -i 's/CONFIG_CUSTOM_KERNEL_IMGSENSOR=*.*/CONFIG_CUSTOM_KERNEL_IMGSENSOR=\"imx586p2q2_mipi_raw s5k3p9sp_mipi_raw hi846p2q_mipi_raw ov02a10p2q_mipi_mono gc02m0p2q_mipi_mono ov02a10p2q_mipi_mono1 gc02m0p2q_mipi_mono1\"/g' $(KERNEL_CONFIG_FILE))
-endif
-#endif
-
-#ifdef VENDOR_EDIT
-#ZhengQiong@camera,driver, 2019/12/27, add for 19551 camera config
-ifneq ($(filter oppo6779_19551, $(OPPO_TARGET_DEVICE)),)
-$(shell sed -i 's/CONFIG_CUSTOM_KERNEL_IMGSENSOR=*.*/CONFIG_CUSTOM_KERNEL_IMGSENSOR=\"imx586_mipi_raw s5kgw1_mipi_raw s5kgh1_mipi_raw s5k3m5_mipi_raw gc8054_mipi_raw gc02m0_mipi_mono gc02m0f_mipi_mono\"/g' $(KERNEL_CONFIG_FILE))
-endif
-#endif
-
 ifeq ($(wildcard $(TARGET_PREBUILT_KERNEL)),)
 KERNEL_MAKE_DEPENDENCIES := $(shell find $(KERNEL_DIR) -name .git -prune -o -type f | sort)
 KERNEL_MAKE_DEPENDENCIES := $(filter-out %/.git %/.gitignore %/.gitattributes,$(KERNEL_MAKE_DEPENDENCIES))
