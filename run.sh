e994236022a170ef150ac3831cff8607ef5ee666
picasso09 <sayazaki282901@gmail.com>
2024-01-21 17:06:56 +0000

    tools: dct:use python3 thx for this commit: https://github.com/Mayuri-Chan/android_kernel_xiaomi_mt6765/commit/3a21bbcf3db364fca4f8fdf84f002ffbcdf8007b

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export KBUILD_BUILD_USER="picasso"
-export PATH="/root/Dv-kernelRMX2189Q/clang/bin:$PATH"
+export PATH="$(pwd)/clang/bin:$PATH"
     make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
                           CC=clang \
