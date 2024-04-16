fea70b76da903f5f7fb3a99c297b24de3a25d351
picasso09 <sayazaki282901@gmail.com>
2024-04-16 04:16:25 +0000

    RMX2185: defconfig helium
    Signed-off-by: picasso09

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export KBUILD_BUILD_USER="picasso"
 export PATH="$(pwd)/clang/bin:$PATH"
-    make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
+    make -j$(nproc --all) O=out ARCH=arm64 oppo6765_helium_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
                           CC=clang \
                           CROSS_COMPILE=aarch64-linux-gnu- \
