78f75f838a88ec1f558851c12f4920b20f9b2cca
picasso09 <sayazaki282901@gmail.com>
2024-06-16 09:41:58 +0000

    RMX2185: tracked KSU 0.9.5
    Signed-off-by: picasso09

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
-export KBUILD_BUILD_USER="picasso"
+#!/bin/bash
+
+# set environment variables
+#git clone --depth=1 https://github.com/picasso09/proton-clang clang
+export KBUILD_BUILD_HOST="DV-WORK"
+export KBUILD_BUILD_USER="picasso09"
 export PATH="$(pwd)/clang/bin:$PATH"
-    make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
-    make -j$(nproc --all) ARCH=arm64 O=out \
-                          CC=clang \
-                          CROSS_COMPILE=aarch64-linux-gnu- \
-                          CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+# build kernel
+make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
+make -j$(nproc --all) ARCH=arm64 O=out \
+                      HOSTCC="ccache clang" \
+                      HOSTCXX="ccache clang++" \
+                      CC="ccache clang" \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi-
