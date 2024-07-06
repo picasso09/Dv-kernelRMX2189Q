3e6eabbe8c210aca1a933a52dfa96262b9272427
picasso09 <sayazaki282901@gmail.com>
2024-07-06 14:28:51 +0000

    RMX2185: add dtbo building logic
    Signed-off-by: picasso09
    
    Date:      Sat Jul 6 14:28:51 2024 +0000
    Change-Id: 7c09e3383f224e4ae2fa14f748ea03cfac47eed7

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
-export KBUILD_BUILD_USER="picasso"
+#!/bin/bash
+
+# set environment variables
+git clone --depth=1 https://github.com/picasso09/clang-9.0.3-r353983c1 clang
+#mkdir clang && cd clang2 && wget https://android.googlesource.com/platform//prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r522817.tar.gz && tar xf clang-r522817.tar.gz && rm clang-r522817.tar.gz && cd ..
+export KBUILD_BUILD_HOST="DV-WORK"
+export KBUILD_BUILD_USER="picasso09"
 export PATH="$(pwd)/clang/bin:$PATH"
-    make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
-    make -j$(nproc --all) ARCH=arm64 O=out \
-                          CC=clang \
-                          CROSS_COMPILE=aarch64-linux-gnu- \
-                          CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+# build kernel
+make -j$(nproc --all) O=out ARCH=arm64 oppo6765_defconfig
+make -j16 ARCH=arm64 O=out \
+                      HOSTCC="ccache clang" \
+                      HOSTCXX="ccache clang++" \
+                      CC="ccache clang" \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi-
